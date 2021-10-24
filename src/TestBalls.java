56b34d7c8ebbddb0aa67a7789332cdf3bd2e82b2
cassim <contact@aituglo.com>
2021-10-24 10:58:35 +0200

    Add Balls

---
+++ src/TestBalls.java
@@ -0,0 +1,17 @@
+public class TestBalls {
+    public static void main(String[] args) {
+
+        Balls balls = new Balls(5);
+
+        System.out.println(balls);
+
+        balls.translate(2, 0);
+
+        System.out.println(balls);
+
+        balls.reInit();
+
+        System.out.println(balls);
+
+    }
+}
