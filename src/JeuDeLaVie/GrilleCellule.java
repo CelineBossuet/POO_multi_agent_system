d34c4fbef44e0da155c9507d0a01b119f540fd85
fryspace <titou.lecamp@gmail.com>
2021-10-26 12:31:02 +0200

    finalisation jeu de la vie

--- src/JeuDeLaVie/GrilleCellule.java
+++ src/JeuDeLaVie/GrilleCellule.java
@@ -1,43 +1,63 @@
 package JeuDeLaVie;
 
 import gui.GUISimulator;
+import gui.Rectangle;
 import gui.Simulable;
+
+import java.awt.*;
 
 public class GrilleCellule implements Simulable {
 
-    private int n;
-    private int m;
+    private final int n;
+    private final int m;
     private Cellule[][] tab;
-    private GUISimulator window;
+    private final GUISimulator window;
+    private  final int[][] init;
 
 
-    public GrilleCellule(int n, int m, GUISimulator window) {
+    public GrilleCellule(int n, int m, gui.GUISimulator window, boolean start, int[][] init) {
         this.n = n;
         this.m = m;
         this.tab = new Cellule[n][m];
         this.window = window;
+        this.init = init;
+        if(start) {
+            this.restart();
+        }else{
+            this.restartVierge();
+        }
+    }
+
+    private void restartVierge() {
+        this.window.reset();
+        for(int i = 0; i< this.n; i++){
+            for(int j = 0; j < this.m; j++){
+                this.tab[i][j] = new Cellule(i,j,false);
+                window.addGraphicalElement(new Rectangle(10*i, 10* j, Color.BLACK, Color.WHITE, 10));
+            }
+        }
     }
 
     public int getNbVoisin(Cellule cel){
         int x = cel.getX();
         int y = cel.getY();
         int compteur = 0;
-        if(this.tab[(x-1)%this.n][(y-1)%this.m].isEstVivante()){
+        if(this.tab[(x-1+this.n)%this.n][(y-1 +this.n)%this.m].isEstVivante()){
             compteur+=1;
         }
-        if(this.tab[(x)%this.n][(y-1)%this.m].isEstVivante()){
+        if(this.tab[(x)%this.n][(y-1+this.n)%this.m].isEstVivante()){
             compteur+=1;
         }
-        if(this.tab[(x+1)%this.n][(y-1)%this.m].isEstVivante()){
+        if(this.tab[(x+1)%this.n][(y-1+this.n)%this.m].isEstVivante()){
             compteur+=1;
         }
-        if(this.tab[(x-1)%this.n][(y)%this.m].isEstVivante()){
+        if(this.tab[(x-1+this.n)%this.n][(y)%this.m].isEstVivante()){
             compteur+=1;
         }
         if(this.tab[(x+1)%this.n][(y)%this.m].isEstVivante()){
             compteur+=1;
         }
-        if(this.tab[(x-1)%this.n][(y+1)%this.m].isEstVivante()){
+        if(this.tab[(x-1+this.n)%this.n][(y+1)%this.m].isEstVivante()){
             compteur+=1;
         }
         if(this.tab[(x)%this.n][(y+1)%this.m].isEstVivante()){
@@ -50,7 +70,7 @@ public class GrilleCellule implements Simulable {
     }
 
     GrilleCellule nextStep(){
-        GrilleCellule new_grille = new GrilleCellule(this.n, this.m, GUISimulator window);
+        GrilleCellule new_grille = new GrilleCellule(this.n, this.m, this.window, false, this.init);
         for (int i = 0; i < this.n; i++){
             for(int j = 0; j < this.m; j++){
                 if(this.tab[i][j].isEstVivante()){
@@ -61,17 +81,12 @@ public class GrilleCellule implements Simulable {
                     else if(getNbVoisin(this.tab[i][j]) == 2 || getNbVoisin(this.tab[i][j]) ==3){
                         new_grille.tab[i][j].setEstVivant(true);
                     }
-                    else if(getNbVoisin(this.tab[i][j]) > 4) {
+                    else if(getNbVoisin(this.tab[i][j]) > 3) {
                         new_grille.tab[i][j].setEstVivant(false);
                     }
                 }
                 else{
-                    if(getNbVoisin(this.tab[i][j]) == 3){
-                        new_grille.tab[i][j].setEstVivant(true);
-                    }
-                    else{
-                        new_grille.tab[i][j].setEstVivant(false);
-                    }
+                    new_grille.tab[i][j].setEstVivant(getNbVoisin(this.tab[i][j]) == 3);
                 }
             }
         }
@@ -82,11 +97,31 @@ public class GrilleCellule implements Simulable {
     @Override
     public void next() {
         GrilleCellule new_grille_cellule=this.nextStep();
+        this.tab = new_grille_cellule.tab;
+        for (int i = 0; i < this.n ; i++) {
+            for (int j = 0; j < this.m; j++) {
+                if(this.tab[i][j].isEstVivante()){
+                    window.addGraphicalElement(new Rectangle(10*j, 10* i, Color.BLACK, Color.BLACK, 10));
+                }else{
+                    window.addGraphicalElement(new Rectangle(10*j, 10* i, Color.BLACK, Color.WHITE, 10));
+                }
+            }
 
+        }
     }
 
     @Override
     public void restart() {
-
+        this.window.reset();
+        for(int i = 0; i< this.n; i++){
+            for(int j = 0; j < this.m; j++){
+                this.tab[i][j] = new Cellule(i,j,false);
+                window.addGraphicalElement(new Rectangle(10*j, 10* i, Color.BLACK, Color.WHITE, 10));
+            }
+        }
+        for (int i = 0; i < this.init[0].length; i++) {
+            this.tab[this.init[0][i]][this.init[1][i]].setEstVivant(true);
+            window.addGraphicalElement(new Rectangle(10*this.init[1][i], 10* this.init[0][i], Color.BLACK, Color.BLACK, 10));
+        }
     }
 }
--- src/JeuDeLaVie/TestJeuDeLaVie.java
+++ src/JeuDeLaVie/TestJeuDeLaVie.java
@@ -1,9 +1,23 @@
 package JeuDeLaVie;
 
+import gui.GUISimulator;
+
+import java.awt.*;
+
 public class TestJeuDeLaVie {
 
-    public static void main(String[] args) {
 
+    private static Object Cellule;
+
+    public static void main(String[] args) {
+        int n = 500;
+        int m = 500;
+        int nSim= 500/10;
+        int mSim = 500/10;
+        //int[][] init = {{2,2,2,2,2,2,2,2},{2,3,4,5,6,7,8,9,10}}; etat stationnaire
+        int[][] init = {{2,2,2},{2,3,4}}; //etat clignotant
+        GUISimulator gui = new GUISimulator(n, m, Color.BLACK);
+        gui.setSimulable(new GrilleCellule(nSim, mSim, gui, true, init));
     }
 
 }
